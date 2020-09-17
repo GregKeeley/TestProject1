aaebf35d0b0e86e9f79280184a5ab588a3d28811
Gregory Keeley <GKeeley86@gmail.com>
2020-09-17 13:41:55 -0400

    - Initial push

--- TestProject1/ViewController.swift
+++ TestProject1/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    let fellows63 = ["Ameni","Cameron","Casandra","Jaheed","Juan","Greg","Oscar","Luba","Yuliia","Alex","Brendon","Mai","Chelsi","Christian","Eric","Kelby","Margiett","Bienbenido","Matt", "Shaniya", "Tanya", "Tiffany", "Tsering", ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func randomFellowButtonPressed(_ sender: UIButton) {
+        helloLabel.text = ("Hello, \(fellows63.randomElement() ?? "Person")!")
+    }
+    
 }
 
